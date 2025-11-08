f6d96b0c855864db7fbea8796e1e78c1b97bca2e
John M Sweeney <johnmsweeney@gmail.com>
2025-11-07 19:01:14 -0500

    Proof of Concept for SQ Midi interface

---
+++ automation/sq_midi_control/midi_notes.py
@@ -0,0 +1,47 @@
+"""
+MIDI Note Definitions for SQ-Rack SoftKey Triggers.
+
+These constants map common notes to their MIDI note number,
+based on the standard mapping where Middle C (C4) is 60 (0x3C).
+The SQ-Rack typically starts SoftKeys around C3 (Note 48).
+"""
+
+# --- OCTAVE 3 (C3 to B3) ---
+
+# C3 (MIDI Note Number 48, Hex 0x30)
+NOTE_C3 = 48 
+# C#3 (MIDI Note Number 49, Hex 0x31)
+NOTE_CSHARP3 = 49 
+# D3 (MIDI Note Number 50, Hex 0x32)
+NOTE_D3 = 50 
+# D#3 (MIDI Note Number 51, Hex 0x33)
+NOTE_DSHARP3 = 51 
+# E3 (MIDI Note Number 52, Hex 0x34)
+NOTE_E3 = 52
+# F3 (MIDI Note Number 53, Hex 0x35)
+NOTE_F3 = 53
+# F#3 (MIDI Note Number 54, Hex 0x36)
+NOTE_FSHARP3 = 54
+# G3 (MIDI Note Number 55, Hex 0x37)
+NOTE_G3 = 55
+# G#3 (MIDI Note Number 56, Hex 0x38)
+NOTE_GSHARP3 = 56
+# A3 (MIDI Note Number 57, Hex 0x39)
+NOTE_A3 = 57
+# A#3 (MIDI Note Number 58, Hex 0x3A)
+NOTE_ASHARP3 = 58
+# B3 (MIDI Note Number 59, Hex 0x3B)
+NOTE_B3 = 59
+
+# --- OCTAVE 4 (C4/Middle C and above) ---
+
+# C4 / Middle C (MIDI Note Number 60, Hex 0x3C)
+NOTE_C4 = 60
+# C#4 (MIDI Note Number 61, Hex 0x3D)
+NOTE_CSHARP4 = 61
+# D4 (MIDI Note Number 62, Hex 0x3E)
+NOTE_D4 = 62
+# D#4 (MIDI Note Number 63, Hex 0x3F)
+NOTE_DSHARP4 = 63
+# E4 (MIDI Note Number 64, Hex 0x40)
+NOTE_E4 = 64
